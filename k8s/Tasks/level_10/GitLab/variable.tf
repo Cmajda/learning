3a7333da691538723db6b7a72e34a05251ea02f9
Cmajda <cmajdalkam@gmail.com>
2021-09-22 21:15:05 +0200

    firts init

---
+++ k8s/Tasks/level_10/GitLab/variable.tf
@@ -0,0 +1,14 @@
+variable "projectname" {
+  default = "HelloCiCd"
+}
+variable "location" {
+  default = "northeurope"
+}
+
+variable "aks_node_count" {
+  default = "1"
+}
+
+variable "acr_sku" {
+  default = "Basic"
+}
